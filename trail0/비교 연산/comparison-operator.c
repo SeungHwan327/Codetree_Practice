c9b22970a9afdb83d669ef1902c75faf19ce3235
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-06-24 16:52:07 +0900

    [Passed] 비교 연산 (1ms, 0MB)

---
+++ trail0/비교 연산/comparison-operator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    if(a>=b) puts("1");
+    else puts("0");
+
+    if(a>b) puts("1");
+    else puts("0");
+
+    if(a<=b) puts("1");
+    else puts("0");
+
+    if(a<b) puts("1");
+    else puts("0");
+
+    if(a==b) puts("1");
+    else puts("0");
+
+    if(a!=b) puts("1");
+    else puts("0");
+
+    
+    return 0;
+}
