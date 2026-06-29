6a6b4ec6adf36d0b251e1414b8ff2d1941750deb
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-06-29 13:37:34 +0900

    [Passed] M으로 계속 나누기 (1ms, 0MB)

---
+++ trail0/M으로 계속 나누기/continue-dividing-by-m.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    
+    printf("%d\n",N);
+
+    while(N>=M) {
+        printf("%d\n",N/M);
+        N = N/M;
+    }
+    
+    return 0;
+}
