7088b8aed0935ea2f44a7aceb6bda55469c3e646
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-07-03 16:35:17 +0900

    [Passed] 별표 출력하기 2 (1ms, 0MB)

---
+++ trail0/별표 출력하기 2/print-star-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+
+    for(int i=0; i<N; i++) {
+        for(int j=N-i; j>0; j--) {
+            printf("* ");
+        }
+        puts("");
+    }
+
+    return 0;
+}
