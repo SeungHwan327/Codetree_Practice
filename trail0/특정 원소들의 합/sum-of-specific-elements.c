e2c0e9d1b5f53dcd0eeb4155eee84bbf4b55c2a3
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-07-04 20:45:13 +0900

    [Passed] 특정 원소들의 합 (1ms, 0MB)

---
+++ trail0/특정 원소들의 합/sum-of-specific-elements.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int arr[4][4];
+    int sum=0;
+
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<4; j++) {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<=i; j++) {
+            sum += arr[i][j];
+        }
+    }
+
+    printf("%d",sum);
+
+    return 0;
+}
