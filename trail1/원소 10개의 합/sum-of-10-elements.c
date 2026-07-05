2a7e4636156d31b746983800b94168086a0aa7f8
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-07-05 19:35:01 +0900

    [Passed] 원소 10개의 합 (2ms, 0MB)

---
+++ trail1/원소 10개의 합/sum-of-10-elements.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int sum=0;
+    for(int i=0; i<10; i++) {
+        scanf("%d",&n);
+        sum += n;
+    }
+
+    printf("%d",sum);
+    return 0;
+}
