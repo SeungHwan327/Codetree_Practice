9904232bde66ef5c3c083add389ffd056285179f
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-07-05 19:38:58 +0900

    [Passed] 배열 채우고 출력하기 (2ms, 0MB)

---
+++ trail1/배열 채우고 출력하기/filling-array-and-print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+
+    for(int i=0; i<10; i++) {
+        scanf("%c ",&arr[i]);
+    }
+
+    
+    for(int i=0; i<10; i++) {
+        printf("%c",arr[9-i]);
+    }
+    return 0;
+}
