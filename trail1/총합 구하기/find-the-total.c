3dd40898b1a737525b1871cc71cc047d8c8b1f4b
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-07-25 23:28:32 +0900

    [Passed] 총합 구하기 (1ms, 0MB)

---
+++ trail1/총합 구하기/find-the-total.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int A,B,sum=0;
+    scanf("%d %d",&A,&B);
+    
+    for(int i=A; i<=B; i++) {
+        if(i%6==0 && i%8!=0) sum+=i;
+    }
+
+    printf("%d",sum);
+    return 0;
+}
