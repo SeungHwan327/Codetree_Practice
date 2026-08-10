47a85d05c93e60c12625111eda8f6240bd8a2e90
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-08-11 03:07:16 +0900

    [Passed] 열 단위로 반복하여 출력 (1ms, 0MB)

---
+++ trail1/열 단위로 반복하여 출력/print-in-column.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            printf("%d",i+1);
+        }
+        puts("");
+    }
+    return 0;
+}
