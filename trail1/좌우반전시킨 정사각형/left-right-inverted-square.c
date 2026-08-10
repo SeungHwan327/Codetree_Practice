a095b48fc1df5cc52a75f2d12f37ab0829e03d6b
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-08-11 03:24:51 +0900

    [Passed] 좌우반전시킨 정사각형 (1ms, 0MB)

---
+++ trail1/좌우반전시킨 정사각형/left-right-inverted-square.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+
+    for(int i=0; i<N; i++) {
+        for(int j=N; j>0; j--) {
+            printf("%d ",j*(i+1));
+        }
+        puts("");
+    }
+    return 0;
+}
