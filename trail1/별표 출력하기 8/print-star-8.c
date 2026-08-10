b44c70010bddb6b52d299f13496eef2c0e6145af
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-08-10 16:19:46 +0900

    [Passed] 별표 출력하기 8 (2ms, 0MB)

--- trail1/별표 출력하기 8/print-star-8.c
+++ trail1/별표 출력하기 8/print-star-8.c
@@ -7,8 +7,6 @@ int main() {
     for(int i=1; i<=N; i++) {
         if(i%2!=0) {
             printf("* ");
-            puts("");
-            continue;
         } else {
             for(int j=0; j<i; j++) {
                 printf("* ");
