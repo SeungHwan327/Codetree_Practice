63ac3164c764bd01475bf3945f2ddc489d2fee7e
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-08-10 16:11:39 +0900

    [Passed] 체크 다이아몬드 (1ms, 0MB)

---
+++ trail1/체크 다이아몬드/check-diamond.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int N; 
+    scanf("%d",&N);
+
+    for(int i=0; i<N; i++) {
+        for(int j=N-i-1; j>0; j--) {
+            printf(" ");
+        }
+        for(int j=0; j<i+1; j++) {
+            printf("* ");
+        }
+        puts("");
+    }
+
+    for(int i=0; i<N-1; i++) {
+        for(int j=0; j<i+1; j++) {
+            printf(" ");
+        }
+        for(int j=N-i-1; j>0; j--) {
+            printf("* ");
+        }
+        puts("");
+    }
+    return 0;
+}
