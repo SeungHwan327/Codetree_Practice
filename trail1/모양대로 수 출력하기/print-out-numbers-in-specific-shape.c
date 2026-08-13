a6ff090aedcdb57ff3fa6b6dfa65ce1140c97675
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-08-13 15:34:23 +0900

    [Passed] 모양대로 수 출력하기 (1ms, 0MB)

---
+++ trail1/모양대로 수 출력하기/print-out-numbers-in-specific-shape.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<i; j++) {
+            printf("  ");
+        }
+        for(int j=N-i; j>0; j--) {
+            printf("%d ",j);
+        }
+        puts("");
+    }
+    return 0;
+}
