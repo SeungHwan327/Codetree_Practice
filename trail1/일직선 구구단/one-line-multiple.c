20ef5aff71227b938d0cb9ffa91ed8009cc2093b
SeungHwan327 <100602010+SeungHwan327@users.noreply.github.com>
2026-08-13 16:07:03 +0900

    [Passed] 일직선 구구단 (1ms, 0MB)

---
+++ trail1/일직선 구구단/one-line-multiple.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=N; j++) {
+            printf("%d * %d = %d",i,j,i*j);
+            puts("");
+        }
+    }
+    return 0;
+}
